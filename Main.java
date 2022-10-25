f5c5d38e8681d8ae3a5a96c6eed17923e846ef6a
Brendan Gass <94894839+BrendanGass@users.noreply.github.com>
2022-10-25 11:23:10 +0000

    deletion of clutter

--- Main.java
+++ Main.java
@@ -7,5 +7,6 @@ class Main {
   public static void main(String[] args) {
     Start gui = new Start();
     gui.showDialog();
+
   }
 }
--- Start.java
+++ Start.java
@@ -8,6 +8,7 @@ import javax.swing.JTextField;
 public class Start extends JFrame implements ActionListener {
 
   JButton startgame = new JButton("Start");
+  JButton rules = new JButton("Rules");
   JButton quit = new JButton("Quit");
   JLabel player1Label = new JLabel("Player 1");
   JLabel player2Label = new JLabel("Player 2");
@@ -22,38 +23,47 @@ public class Start extends JFrame implements ActionListener {
   public void showDialog() {
     setLayout(null);
 
-    player1.setBounds(10, 40, 250, 20);
-    player2.setBounds(310, 40, 250, 20);
-    player1Label.setBounds(10, 20, 250, 20);
-    player2Label.setBounds(310, 20, 250, 20);
-    startgame.setBounds(35, 70, 200, 40);
-    quit.setBounds(335, 70, 200, 40);
+    player1.setBounds(15, 100, 250, 20);
+    player2.setBounds(15, 150, 250, 20);
+    player1Label.setBounds(15, 80, 250, 20);
+    player2Label.setBounds(15, 130, 250, 20);
+    startgame.setBounds(35, 185, 200, 40);
+    rules.setBounds(35, 240, 200, 40);
+    quit.setBounds(35, 295, 200, 40);
 
     this.add(player1);
     this.add(player2);
     this.add(player1Label);
     this.add(player2Label);
     this.add(startgame);
+    this.add(rules);
     this.add(quit);
 
     startgame.addActionListener(this);
+    rules.addActionListener(this);
     quit.addActionListener(this);
   
-    setTitle("Welcome to CyberDice");
+    setTitle("CyberDice");
     setDefaultCloseOperation(EXIT_ON_CLOSE);
-    setSize(570,300);
+    setSize(280,400);
     setVisible(true); 
 
     }
 
   public void actionPerformed(ActionEvent e) {
     if (e.getSource() == startgame) {
-      new GameView();
+      name1 = player1.getText();
+      name2 = player2.getText();
+      GameView gameView = new GameView(name1, name2);
+      dispose();
+      
     } else if (e.getSource() == quit) {
       dispose();
+    } else if (e.getSource() == rules) {
+      Rules gui = new Rules();
+      gui.showDialog();;
     }
- 
-
+    
   }
-  
+
 }
